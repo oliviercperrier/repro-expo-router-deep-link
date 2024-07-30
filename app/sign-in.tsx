580e8384cbed442ec81a98d0727d61cd8741d94d
oliviercperrier <olivier.cperrier@hotmail.com>
2024-07-29 21:40:14 -0400

    init

---
+++ app/sign-in.tsx
@@ -0,0 +1,25 @@
+import { router } from "expo-router";
+import { Text, View } from "react-native";
+
+import { useSession } from "../ctx";
+
+export default function SignIn() {
+  const { signIn } = useSession();
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text
+        onPress={() => {
+          signIn();
+          // Navigate after signing in. You may want to tweak this to ensure sign-in is
+          // successful before navigating.
+          router.navigate("/profile");
+        }}
+        style={{
+          color: "white",
+        }}
+      >
+        Sign In
+      </Text>
+    </View>
+  );
+}
